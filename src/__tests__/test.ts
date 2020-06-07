87a9865e1d62b12fd2961b91e18c07f382c1698e
hassoun86 <husseinfarhat1@hotmail.com>
2020-06-07 15:55:17 +0200

    Add service locator package and test

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,4 +1,55 @@
-import { ServiceLocator } from '../index';
-test('My Greeter', () => {
-  expect(ServiceLocator()).toBe('Hello');
+import serviceLocator from '../index';
+jest.mock('../index');
+
+class Service {}
+
+let service: Service;
+
+beforeEach(() => {
+  service = new Service();
 });
+
+afterEach(() => {
+  serviceLocator.clear();
+});
+
+it('Should successfully register a dependency', () => {
+  spyOn(serviceLocator, 'register');
+  serviceLocator.register('service', () => {
+    return service;
+  });
+
+  expect(serviceLocator.register).toHaveBeenCalledTimes(1);
+});
+
+it('Should successfully get a dependency', () => {
+  spyOn(serviceLocator, 'get').and.returnValue(service);
+  serviceLocator.register('service', () => {
+    return service;
+  });
+
+  serviceLocator.get('service');
+
+  expect(serviceLocator.get).toHaveBeenCalledTimes(1);
+  expect(serviceLocator.get).toHaveBeenCalledWith('service');
+});
+
+it('Should successfully get all dependencies', () => {
+  spyOn(serviceLocator, 'getAll');
+  serviceLocator.register('service', () => {
+    return service;
+  });
+
+  serviceLocator.getAll();
+  expect(serviceLocator.getAll).toHaveBeenCalledTimes(1);
+});
+
+it('Should successfully clear all dependencies', () => {
+    spyOn(serviceLocator, 'clear');
+    serviceLocator.register('service', () => {
+      return service;
+    });
+  
+    serviceLocator.clear();
+    expect(serviceLocator.clear).toHaveBeenCalledTimes(1);
+  });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,78 @@
-export const ServiceLocator = () => 'Hello';
+import DependencyValue from './interfaces';
+
+class ServiceLocator {
+    private dependencyMap: DependencyValue;
+    private dependencyCache: DependencyValue;
+    constructor() {
+        this.dependencyMap = {};
+        this.dependencyCache = {};
+    }
+
+    /**
+     * @Method Returns {Void}
+     * @param dependencyName 
+     * @param constructor 
+     */
+    public register(dependencyName: string, constructor: Function) : void {
+        if (typeof constructor !== 'function') {
+            throw new Error(dependencyName + ': Dependency constructor is not a function');
+        }
+
+        if (!dependencyName) {
+            throw new Error('Invalid dependency name provided');
+        }
+
+        this.dependencyMap[dependencyName] = constructor;
+    }
+
+    /**
+     * @Method Returns {Function}
+     * @param dependencyName {String}
+     */
+    public get(dependencyName: string) : Function {
+        if (this.dependencyMap[dependencyName] === undefined) {
+            throw new Error(dependencyName + ': Attempting to retrieve unknown dependency');
+        }
+
+        if (typeof this.dependencyMap[dependencyName] !== 'function') {
+            throw new Error(dependencyName + ': Dependency constructor is not a function');
+        }
+
+        if (this.dependencyCache[dependencyName] === undefined) {
+            let dependencyConstructor = this.dependencyMap[dependencyName];
+            let dependency = dependencyConstructor(this);
+
+            if (dependency) {
+                this.dependencyCache[dependencyName] = dependency;
+            }
+        }
+
+        return this.dependencyCache[dependencyName];
+    }
+
+    /**
+     * @Method Returns {DependencyValue}
+     * Get all dependencies
+     */
+    public getAll() : DependencyValue{
+        let dependencies = Object.keys(this.dependencyMap);
+
+        for (let key in dependencies) {
+            this.get(dependencies[key]);
+        }
+
+        return this.dependencyCache;
+    }
+
+    /**
+     * @Method Returns {Void}
+     * Clear all dependencies
+     */
+    public clear() : void {
+        this.dependencyCache = {};
+        this.dependencyMap = {};
+    }
+
+}
+
+export default new ServiceLocator();
